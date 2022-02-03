0001c013c6d4caab19bd60d7f42fd19c89274772
Erwin Angeles <erwinangeles@live.com>
2022-02-03 13:30:18 -0600

    logo fix 5

--- resources/views/emails/formcapture.blade.php
+++ resources/views/emails/formcapture.blade.php
@@ -133,7 +133,7 @@
 <div style="border-top:0px solid transparent; border-left:0px solid transparent; border-bottom:0px solid transparent; border-right:0px solid transparent; padding-top:0px; padding-bottom:0px; padding-right: 0px; padding-left: 0px;">
 <!--<![endif]-->
 <div align="center" class="img-container center autowidth" style="padding-right: 0px;padding-left: 0px;">
-<img src="{{env("LOGO")}}" alt="Logo" title="Logo" style="display:block" width="200" height="87" />
+<div style="height: 87px; width: 200px; display: block; background: url({{env("LOGO")}}); background-size: contain;"></div>
 
 </div>
 </div>
